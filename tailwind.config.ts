9d3eb059bea777a6ab66080ffde28e05e6d9668a
OscarGiraldo <oscar.giraldo@linktic.com>
2024-12-03 23:55:20 -0500

    bulid:adding tailwindcss

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+import {nextui} from "@nextui-org/react";
+
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      flex: {
+        full: "0 0 100%"
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()]
+} satisfies Config;
+
+
