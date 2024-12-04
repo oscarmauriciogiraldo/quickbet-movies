fdcc0fdc4e7befe92888f6498741683943749e2a
OscarGiraldo <oscar.giraldo@linktic.com>
2024-12-04 00:56:31 -0500

    styles: config font project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const ibm = IBM_Plex_Sans({ subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700"] });
 
 export const metadata: Metadata = {
   title: "Quickbet Movies",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ibm.className}>{children}</body>
     </html>
   );
 }
