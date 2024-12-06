cefdef7549dc974f2d589f8a23750353e2913793
OscarGiraldo <oscar.giraldo@linktic.com>
2024-12-05 21:34:33 -0500

    feat: implement search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,41 @@
+import { getNowPlaying, getPopularMovies, getTopRatedMovies, getUpcomingMovies } from "@/lib/getMovies";
 import Banner from "./components/banner/Banner";
 import RootLayout from "./layout";
+import SearchComponent from "./components/search-component/SearchComponent";
 
-export default function Home(): JSX.Element {
+export default async function Home() {
+  const nowPlayingMovies = await getNowPlaying();
+  const upcomingMovies = await getUpcomingMovies();
+  const topRatedMovies = await getTopRatedMovies();
+  const popularMovies = await getPopularMovies();
+
   return (
     <RootLayout>
       <div className="bodyContainer">
         <div className="banner flex w-full h-[436px] justify-center lg:container lg:mx-auto">
           <Banner></Banner>
         </div>
+        <main className="flex flex-row row-start-2 items-center sm:items-start lg:container lg:mx-auto">
+        {/* section left search and select genres */}
+        <div className="boxLeft h-screen flex flex-col w-[25%] px-[10px] gap-8">
+          
+          <SearchComponent/>
+
+          <div className="flex flex-col ">
+            <div className="title">
+              <h3>Genres</h3>
+            </div>
+            <div className="flex">
+              
+            </div>
+          </div>
+        </div>
+
+        {/* Content movies */}
+        <div className="boxRight bg-[--cardContianer-backround] flex flex-col w-[75%] px-2">
+          
+        </div>
+      </main>
       </div>
     </RootLayout>
   );
